a6777c6f3a126ab945bd2a372f7d3b7917a5ff4e
Gazal <mohamed.gazal@gmail.com>
2015-07-30 15:51:29 +0530

    moving src files to src folder, using gulp-inline-source to generate single html

--- gulpfile.js
+++ gulpfile.js
@@ -4,19 +4,30 @@
 var gulp = require('gulp');
 var $ = require('gulp-load-plugins')();
 var runSequence = require('run-sequence');
+var inlinesource = require('gulp-inline-source');
 
 gulp.task('jshint', function () {
   return gulp.src([
-      '*.js',
-      '*.html'
+      'src/*.js',
+      'src/*.html'
     ])
     .pipe($.jshint.extract()) // Extract JS from .html files
     .pipe($.jshint())
     .pipe($.jshint.reporter('jshint-stylish'));
 });
 
+gulp.task('inlinesource', function () {
+  var options = {
+    compress: false
+  };
+  return gulp.src('./src/*.html')
+    .pipe(inlinesource(options))
+    .pipe(gulp.dest('./'));
+});
+
 gulp.task('default', function (callback) {
   runSequence(
     'jshint',
+    'inlinesource',
     callback);
 });
